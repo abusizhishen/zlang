99a151eda6d7b36539a95f192778a7769f4e71f2
abusizhishen <abusizhishen@gmail.com>
2022-07-10 00:13:54 +0800

    token

---
+++ token/token.go
@@ -0,0 +1,54 @@
+package token
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+type TokenType string
+
+const (
+	ASSIGN   TokenType = "="
+	PLUS               = "+"
+	MINUS              = "-"
+	BANG               = "!"
+	ASTERISK           = "*"
+	SLASH              = "/"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LT = "<"
+	GT = ">"
+
+	String   = "STRING"
+	Number   = "NUMBER"
+	Identity = "IDENTIFY"
+	Let      = "LET"
+	IF       = "IF"
+	Else     = "ELSE"
+	Return   = "RETURN"
+	Switch   = "SWITCH"
+	FUN      = "FUN"
+
+	EOF     = "EOF"
+	INVALID = "INVALID"
+)
+
+var Keywords = map[string]TokenType{
+	"let":    Let,
+	"if":     IF,
+	"else":   Else,
+	"return": Return,
+	"switch": Switch,
+	"fun":    FUN,
+}
+
+func NewToken(t TokenType, ch byte) Token {
+	return Token{Type: t, Literal: string(ch)}
+}
