5c3cc8143ca02bc0b68a3de22b40078431618b37
abusizhishen <abusizhishen@gmail.com>
2022-07-10 01:05:12 +0800

    repl

--- repl.go
+++ repl.go
@@ -1,1 +1,20 @@
-package zlang
+package main
+
+import (
+	"fmt"
+	"os"
+	"zlang/repl"
+)
+import "os/user"
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Fprintf(os.Stdout, "hello %s !\n", user.Name)
+	fmt.Fprintf(os.Stdout, "feel free to type in command line\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
--- repl/repl.go
+++ repl/repl.go
@@ -1,1 +1,28 @@
 package repl
+
+import (
+	"io"
+	"zlang/lexer"
+	"zlang/token"
+)
+import "bufio"
+import "fmt"
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scaner := bufio.NewScanner(in)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scan := scaner.Scan()
+		if !scan {
+			return
+		}
+
+		line := scaner.Text()
+		lex := lexer.New(line)
+		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
