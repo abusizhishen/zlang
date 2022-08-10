3c34e2316db024d6ad5abcf8d34ffc9a9bcae835
abusizhishen <abusizhishen@gmail.com>
2022-08-10 23:06:13 +0800

    fmt

--- repl.go
+++ repl.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/abusizhishen/zlang/repl"
 	"os"
-	"zlang/repl"
+	"os/user"
 )
-import "os/user"
 
 func main() {
-	user, err := user.Current()
+	u, err := user.Current()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Fprintf(os.Stdout, "hello %s !\n", user.Name)
+	fmt.Fprintf(os.Stdout, "hello %s !\n", u.Name)
 	fmt.Fprintf(os.Stdout, "feel free to type in command line\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
